9c9eb748a0ebc91185b934b36b1011ef17454c32
Easara <easarakulindu@gmail.com>
2026-05-07 21:24:42 +0530

    feat: setup initial processor view data and layout

---
+++ src/pages/ProcessorView.jsx
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero.jsx";
+
+const ProcessorView = ({searchParams,setSearchParams}) => {
+
+    const processorId = searchParams.get("processorId");
+
+    return (
+        <div className="bg-black">
+        </div>
+    );
+}
+export default ProcessorView;
